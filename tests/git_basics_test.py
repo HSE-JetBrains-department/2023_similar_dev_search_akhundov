2e5620e3bab84542aec6869ca976a2fb85f58fc3
TheSeems <me@theseems.ru>
2023-06-16 21:04:52 +0300

    Fix test

--- tests/git_basics_test.py
+++ tests/git_basics_test.py
@@ -33,19 +33,10 @@ class GitBasicsTest(unittest.TestCase):
         langs = repo_info['langs']
         self.assertEqual({'Go': 6}, dict(langs))
         repo_info = extractor.dev_info['bzz@apache.org'][TEST_LANGUAGES_REPO_URL]
-        self.assertEqual({
-            "Go": 147,
-            "Shell": 1,
-            "Scala": 7,
-            "Java": 4,
-            "Makefile": 7,
-            "CSV": 6,
-            "Text": 2,
-            "Go Module": 5,
-            "Go Checksums": 4,
-            "C": 4,
-            "Python": 8
-        }, dict(repo_info['langs']))
+        self.assertEqual(
+            {"Go": 147, "Shell": 1, "Scala": 7, "Java": 4, "Makefile": 7, "CSV": 6,
+             "Text": 11, "C": 4, "Python": 8},
+            dict(repo_info['langs']))
 
     def test_commit_limit(self):
         extractor = RepoInfoExtractor(repo_urls=[TEST_REPO_URL], max_commit_count=1)
