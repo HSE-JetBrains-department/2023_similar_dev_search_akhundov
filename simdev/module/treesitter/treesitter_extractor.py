4a0a0cc334207ae0b6ddb5d58eac182826faf112
TheSeems <me@theseems.ru>
2023-06-18 13:56:51 +0300

    Hone tree-sitter parsing

--- simdev/module/treesitter/treesitter_extractor.py
+++ simdev/module/treesitter/treesitter_extractor.py
@@ -12,7 +12,7 @@ from simdev.util.pipeline import PipelineCache
 
 def _get_repo_build_name(url: str) -> str:
     """
-    Get name of the repository of tree-sitter bindings inside build dir
+    Get name of the repository of tree-sitter grammar inside build dir
     :param url: GitHub repository URL
     :return: name
     """
@@ -24,19 +24,24 @@ def _get_repo_build_name(url: str) -> str:
 
 class TreeSitterExtractor:
     """
-    Stage to prepare tree-sitter downloading and building all bindings needed and
+    Stage to prepare tree-sitter downloading and building all the grammars needed and
     crafting parsers for them
     """
 
-    # Default associations from language name to GitHub Repository of the bindings URL
+    # Default associations from language name to URL of Git repository for its grammar
     # Key must be one of enry classified languages names
-    DEFAULT_LANG_BINDINGS = {
-        'Go': 'https://github.com/tree-sitter/tree-sitter-go',
+    DEFAULT_LANG_GRAMMAR = {
+        'JavaScript': 'https://github.com/tree-sitter/tree-sitter-javascript',
+        'Python': 'https://github.com/tree-sitter/tree-sitter-python',
         'Java': 'https://github.com/tree-sitter/tree-sitter-java',
-        'Python': 'https://github.com/tree-sitter/tree-sitter-python'
+        'Go': 'https://github.com/tree-sitter/tree-sitter-go',
+        'C++': 'https://github.com/tree-sitter/tree-sitter-cpp',
+        'C': 'https://github.com/tree-sitter/tree-sitter-c',
+        'Shell': 'https://github.com/tree-sitter/tree-sitter-bash',
+        'Rust': 'https://github.com/tree-sitter/tree-sitter-rust'
     }
 
-    # Name of the compiled bindings shared object file
+    # Name of the compiled grammar shared object file
     SHARED_OBJECT_FILENAME = "languages.so"
 
     # Tree-sitter language-specific node types for identifiers
@@ -49,13 +54,6 @@ class TreeSitterExtractor:
         "Go": {"identifier", "field_identifier", "type_identifier"},
         "C++": {"identifier", "namespace_identifier", "field_identifier",
                 "type_identifier"},
-        "Ruby": {"identifier", "constant", "symbol"},
-        "TypeScript": {"identifier", "property_identifier",
-                       "shorthand_property_identifier", "type_identifier"},
-        "TSX": {"identifier", "property_identifier",
-                "shorthand_property_identifier", "type_identifier"},
-        "PHP": {"name"},
-        "C#": {"identifier"},
         "C": {"identifier", "field_identifier", "type_identifier"},
         "Shell": {"variable_name", "command_name"},
         "Rust": {"identifier", "field_identifier", "type_identifier"}
@@ -63,49 +61,49 @@ class TreeSitterExtractor:
 
     def __init__(self,
                  build_dir_path: str = "build",
-                 lang_bindings: Optional[Dict[str, str]] = None):
+                 lang_grammars: Optional[Dict[str, str]] = None):
         """
         Initialize tree sitter prepare stage
-        :param build_dir_path: path to directory to store build files for bindings
-        :param lang_bindings: dict from the name of the language
-         to the GitHub repository URL of its tree-sitter bindings.
+        :param build_dir_path: path to directory to store build files for grammars
+        :param lang_grammars: dict from the name of the language
+         to the GitHub repository URL of its tree-sitter grammars.
          Key must be one of enry classified languages names
         """
         self.build_dir_path = Path(build_dir_path)
 
-        self.lang_bindings = lang_bindings
-        if self.lang_bindings is None:
-            # If no binding repos provided use default ones
-            self.lang_bindings = TreeSitterExtractor.DEFAULT_LANG_BINDINGS
+        self.lang_grammars = lang_grammars
+        if self.lang_grammars is None:
+            # If no grammar repos provided use default ones
+            self.lang_grammars = TreeSitterExtractor.DEFAULT_LANG_GRAMMAR
 
-        # dict from language name (e.g. python) to its tree-sitter language object
+        # Dict from language name (e.g. python) to its tree-sitter language object
         self.langs: Dict[str, Language] = {}
-        # dict from language name (e.g. python) to its parser
+        # Dict from language name (e.g. python) to its parser
         self.parsers: Dict[str, Parser] = {}
 
         # Cache language-related functions
         self._build_language_parser = \
             PipelineCache.memory.cache(self._make_language_and_parser)
-        self._clone_language_bindings = \
-            PipelineCache.memory.cache(self._clone_language_bindings)
+        self._clone_language_grammar = \
+            PipelineCache.memory.cache(self._clone_language_grammar)
 
     def prepare(self) -> None:
         """
-        Clone language bindings then build them.
+        Clone language grammars then build them.
         Finally, make parsers for all the languages and store them in the context
         for further use
         """
-        lang_progress = tqdm(self.lang_bindings.keys(), 'Clone tree-sitter bindings')
+        lang_progress = tqdm(self.lang_grammars.keys(), 'Clone tree-sitter grammar')
         for lang in lang_progress:
             lang_progress.set_postfix_str(lang)
-            self._clone_language_bindings(lang)
+            self._clone_language_grammar(lang)
 
         lang_progress.set_description('Building...')
         Language.build_library(output_path=str(self._get_shared_object_file_path()),
                                repo_paths=[str(self._get_repo_build_path(url))
-                                           for url in self.lang_bindings.values()])
+                                           for url in self.lang_grammars.values()])
 
-        for lang in self.lang_bindings:
+        for lang in self.lang_grammars:
             tree_sitter_lang, tree_sitter_lang_parser = \
                 self._make_language_and_parser(lang)
             self.parsers[lang] = tree_sitter_lang_parser
@@ -133,7 +131,7 @@ class TreeSitterExtractor:
         # Getting suitable parser and parse the code
         parser = self.parsers[prog_language]
         parsed_tree = parser.parse(content)
-        counter: Counter[str] = Counter()
+        identifier_counter: Counter[str] = Counter()
 
         def traverse_extract(node: Optional[tree_sitter.Node]):
             """
@@ -147,23 +145,23 @@ class TreeSitterExtractor:
                 if child.type in \
                         TreeSitterExtractor.IDENTIFIER_NODE_TYPES[prog_language]:
                     identifier_name = content[child.start_byte:child.end_byte]
-                    counter.update([identifier_name.decode(encoding='utf-8')])
+                    identifier_counter.update([identifier_name.decode()])
                 traverse_extract(child)
 
         # Extract all the identifiers traversing obtained source code tree
         traverse_extract(parsed_tree.root_node)
-        return counter
+        return identifier_counter
 
-    def _clone_language_bindings(self, lang: str):
+    def _clone_language_grammar(self, lang: str):
         """
-        Clone language bindings repository
-        :param lang: language to clone bindings for
+        Clone language grammar repository
+        :param lang: language to clone grammar for
         """
-        bindings_repo_url = self.lang_bindings[lang]
-        clone_path = self._get_repo_build_path(bindings_repo_url)
+        lang_grammar_repo_url = self.lang_grammars[lang]
+        clone_path = self._get_repo_build_path(lang_grammar_repo_url)
         if not clone_path.exists():
             clone_path.mkdir(parents=True)
-            Repo.clone_from(url=bindings_repo_url, to_path=clone_path)
+            Repo.clone_from(url=lang_grammar_repo_url, to_path=clone_path)
 
     def _make_language_and_parser(self, lang: str) -> Tuple[Language, Parser]:
         """
@@ -171,17 +169,17 @@ class TreeSitterExtractor:
         :param lang: language name to obtain tree-sitter language and parser for
         :return: language and its parser
         """
-        lang_repo_url = self.lang_bindings[lang]
+        lang_grammar_repo_url = self.lang_grammars[lang]
         tree_sitter_lang = Language(
             self._get_shared_object_file_path(),
-            _get_repo_build_name(lang_repo_url))
+            _get_repo_build_name(lang_grammar_repo_url))
         lang_parser = Parser()
         lang_parser.set_language(tree_sitter_lang)
         return tree_sitter_lang, lang_parser
 
     def _get_repo_build_path(self, url: str) -> Path:
         """
-        Get local fs path to repository of tree-sitter bindings inside build dir
+        Get local fs path to repository of tree-sitter grammar inside build dir
         :param url: GitHub repository URL
         :return: path to dir inside build dir
         """
